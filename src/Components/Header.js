e3e5611fe19c63680edce4d9be30db720b3facfb
mdesocio <85764894+mdesocio@users.noreply.github.com>
2021-09-21 15:58:19 -0400

    Added File Directory

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,6 +28,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li><a className="smoothscroll" href="/Files/"></a>Assignments</li>
          </ul>
 
       </nav>
